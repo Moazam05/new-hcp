5dc00cda33080f179cbf1e7caf5ec40f5f8b8751
Moazam05 <salmanmoazam08@gmail.com>
2024-03-30 16:12:35 +0500

    scroll bar

--- src/views/Registration/Components/Agreement.tsx
+++ src/views/Registration/Components/Agreement.tsx
@@ -35,54 +35,62 @@ const Agreement = ({ formik }: AgreementProps) => {
       <Box
         sx={{
           border: "1px solid #000",
-          padding: "8px 10px",
-          fontSize: "14px",
-          color: "#414042",
-          fontWeight: 600,
-          maxHeight: "180px",
-          overflowY: "scroll",
-
-          "&::-webkit-scrollbar": {
-            width: "10px",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            backgroundColor: "#EBEBEB",
-            borderRadius: "8px",
-          },
-          "&::-webkit-scrollbar-track": {
-            borderRadius: "8px",
-            backgroundColor: "transparent",
-          },
-          "@media (max-width: 576px)": {
-            fontSize: "12px",
-            padding: "5px 8px",
-          },
+          padding: "8px 10px ",
         }}
       >
-        <p>Legal Terms for Use of Coherus COMPLETE ™, September 2020</p>
-        <br />
-        <p>
-          Welcome to the CoherusCOMPLETE™ website located at coheruscomplete.com
-          (the “Website”), which is operated by CareMetx, LLC. Please read these
-          Terms of Use (the “Terms”) and Coherus’s Privacy Policy
-          (https://www.coherus.com/privacy-notice) and CareMetx LLC privacy
-          policy at https://caremetx.com/privacy/ carefully because they govern
-          your use of this Website and our online portal that we provide. To
-          make these Terms easier to read, the Site and our services are
-          collectively called the “Services.”
-        </p>
-        <br />
-        <p>
-          website located at coheruscomplete.com (the “Website”), which is
-          operated by CareMetx, LLC. Please read these Terms of Use (the
-          “Terms”) and Coherus’s Privacy Policy
-          (https://www.coherus.com/privacy-notice) and CareMetx LLC privacy
-          policy at https://caremetx.com/privacy/ carefully because they govern
-          your use of this Website and our online portal that we provide. To
-          make these Terms easier to read, the Site and our services are
-          collectively called the “Services.”
-        </p>
+        <Box
+          sx={{
+            fontSize: "14px",
+            color: "#414042",
+            fontWeight: 600,
+            maxHeight: "150px",
+            overflowY: "scroll",
+
+            "&::-webkit-scrollbar": {
+              width: "10px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#EBEBEB",
+              borderRadius: "8px",
+            },
+            "&::-webkit-scrollbar-track": {
+              borderRadius: "8px",
+              backgroundColor: "transparent",
+            },
+            "@media (max-width: 576px)": {
+              fontSize: "12px",
+              padding: "5px 8px",
+            },
+          }}
+        >
+          <div style={{ paddingRight: "5px" }}>
+            <p>Legal Terms for Use of Coherus COMPLETE ™, September 2020</p>
+            <br />
+            <p>
+              Welcome to the CoherusCOMPLETE™ website located at
+              coheruscomplete.com (the “Website”), which is operated by
+              CareMetx, LLC. Please read these Terms of Use (the “Terms”) and
+              Coherus’s Privacy Policy (https://www.coherus.com/privacy-notice)
+              and CareMetx LLC privacy policy at https://caremetx.com/privacy/
+              carefully because they govern your use of this Website and our
+              online portal that we provide. To make these Terms easier to read,
+              the Site and our services are collectively called the “Services.”
+            </p>
+            <br />
+            <p>
+              website located at coheruscomplete.com (the “Website”), which is
+              operated by CareMetx, LLC. Please read these Terms of Use (the
+              “Terms”) and Coherus’s Privacy Policy
+              (https://www.coherus.com/privacy-notice) and CareMetx LLC privacy
+              policy at https://caremetx.com/privacy/ carefully because they
+              govern your use of this Website and our online portal that we
+              provide. To make these Terms easier to read, the Site and our
+              services are collectively called the “Services.”
+            </p>
+          </div>
+        </Box>
       </Box>
+
       <Checkbox
         {...label}
         checked={values.agreement}
