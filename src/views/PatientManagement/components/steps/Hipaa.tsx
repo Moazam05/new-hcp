1415d8a50e8d5944e28bcadd8255f18e3d75f79e
Moazam05 <salmanmoazam08@gmail.com>
2024-05-26 14:21:23 +0500

    download button show

--- src/views/PatientManagement/components/steps/Hipaa.tsx
+++ src/views/PatientManagement/components/steps/Hipaa.tsx
@@ -1,12 +1,21 @@
-import { Box } from "@mui/material";
+import { Box, Checkbox, FormControlLabel } from "@mui/material";
 import * as Yup from "yup";
+import Paragraph from "../../../../components/Paragraph";
+import PrimaryButton from "../../../../components/PrimaryButton";
 
 interface HipaaProps {
   formik: any;
 }
 
 const Hipaa = ({ formik }: HipaaProps) => {
-  const { values, errors, touched, handleChange, handleBlur } = formik;
+  const {
+    values,
+    // errors, touched,
+    handleChange,
+    handleBlur,
+  } = formik;
+
+  console.log("values", values);
 
   return (
     <>
@@ -28,13 +37,84 @@ const Hipaa = ({ formik }: HipaaProps) => {
         <Box
           sx={{
             margin: "50px 0",
+            display: "flex",
+            gap: "50px",
           }}
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At quis
-          sapiente vitae temporibus eligendi debitis amet praesentium, aut quos,
-          eaque enim omnis repellendus modi assumenda fugiat aliquam, rem iusto
-          excepturi.
+          <FormControlLabel
+            control={<Checkbox />}
+            sx={{
+              "& .MuiSvgIcon-root": {
+                fontSize: "35px",
+              },
+            }}
+            componentsProps={{
+              typography: {
+                sx: {
+                  fontSize: "40px",
+                  color: "#00739A",
+                },
+              },
+            }}
+            label="Yes"
+            name="hipaaYes"
+            checked={values.hipaaYes}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+
+          <FormControlLabel
+            control={<Checkbox />}
+            sx={{
+              "& .MuiSvgIcon-root": {
+                fontSize: "35px",
+              },
+            }}
+            componentsProps={{
+              typography: {
+                sx: {
+                  fontSize: "40px",
+                  color: "#00739A",
+                },
+              },
+            }}
+            label="No"
+            name="hipaaNo"
+            checked={values.hipaaNo}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
         </Box>
+
+        {values.hipaaNo && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "20px",
+              margin: "50px 0",
+            }}
+          >
+            <Paragraph
+              sx={{
+                fontSize: "26px",
+                fontWeight: "bold",
+                color: "#00739A",
+              }}
+            >
+              Do you need to download the HIPAA consent form?
+            </Paragraph>
+
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <PrimaryButton>DOWNLOAD</PrimaryButton>
+            </Box>
+          </Box>
+        )}
       </Box>
     </>
   );
@@ -42,8 +122,14 @@ const Hipaa = ({ formik }: HipaaProps) => {
 
 Hipaa.label = "HIPAA";
 
-Hipaa.initialValues = {};
+Hipaa.initialValues = {
+  hipaaYes: false,
+  hipaaNo: false,
+};
 
-Hipaa.validationSchema = {};
+Hipaa.validationSchema = Yup.object().shape({
+  // hipaaYes: Yup.boolean().required("Required"),
+  // hipaaNo: Yup.boolean().required("Required"),
+});
 
 export default Hipaa;
