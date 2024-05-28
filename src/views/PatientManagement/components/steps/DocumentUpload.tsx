f4871ce0392f17bd0f02cc34995263d012340535
Moazam05 <salmanmoazam08@gmail.com>
2024-05-28 22:13:18 +0500

    document upload screen

--- src/views/PatientManagement/components/steps/DocumentUpload.tsx
+++ src/views/PatientManagement/components/steps/DocumentUpload.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { useState } from "react";
 
 const DocumentUpload = () => {
-  return <div>DocumentUpload</div>;
+  const [imageList, setImageList] = useState<any[]>([]);
+
+  //   dont just overwrite the imageList, append to it
+  const handleImageChange = (e: any) => {
+    // setImageList(e.target.files);
+    setImageList([...imageList, ...e.target.files]);
+    // file should be null
+    e.target.value = null;
+  };
+
+  const handleRemoveImage = (index: any) => {
+    const newList = Array.from(imageList);
+    newList.splice(index, 1);
+    setImageList(newList);
+  };
+
+  const handleDrop = (event: any) => {
+    event.preventDefault();
+    const droppedFiles = Array.from(event.dataTransfer.files);
+    setImageList((imageList) => [...imageList, ...droppedFiles]);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <input
+        type="file"
+        name="file"
+        placeholder=""
+        multiple
+        onChange={handleImageChange}
+      />
+
+      <div>
+        {imageList &&
+          Array.from(imageList).map((image: any, index: any) => (
+            // <img key={index} src={URL.createObjectURL(image)} alt="" />
+            <div key={index}>
+              {image?.name}
+              <button onClick={() => handleRemoveImage(index)}>Remove</button>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+
+  //   return <div>DocumentUpload</div>;
 };
 
 export default DocumentUpload;
