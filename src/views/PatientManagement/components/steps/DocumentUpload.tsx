b1b97ee3ed1a082927100397afef5bf717d20fde
Moazam05 <salmanmoazam08@gmail.com>
2024-05-29 02:15:58 +0500

    documents upload

--- src/views/PatientManagement/components/steps/DocumentUpload.tsx
+++ src/views/PatientManagement/components/steps/DocumentUpload.tsx
@@ -4,6 +4,8 @@ import { Box } from "@mui/material";
 import Paragraph from "../../../../components/Paragraph";
 import { UploadImage, UploadImageTwo } from "../../../../assets/images";
 import { GoDotFill } from "react-icons/go";
+import { useState } from "react";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 
 interface DocumentUploadProps {
   formik: FormikProps<any>;
@@ -11,6 +13,7 @@ interface DocumentUploadProps {
 
 const DocumentUpload = ({ formik }: DocumentUploadProps) => {
   const { values, setFieldValue } = formik;
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   const handleImageChange = (e: any) => {
     const selectedFiles = Array.from(e.target.files);
@@ -18,7 +21,7 @@ const DocumentUpload = ({ formik }: DocumentUploadProps) => {
     e.target.value = null;
   };
 
-  const handleRemoveImage = (index: any) => {
+  const handleRemoveImage = (index: number) => {
     const updatedDocuments = values.documents.filter((_, i) => i !== index);
     setFieldValue("documents", updatedDocuments);
   };
@@ -33,124 +36,124 @@ const DocumentUpload = ({ formik }: DocumentUploadProps) => {
     event.preventDefault();
   };
 
-  console.log("values", values?.documents);
+  const bytesToKilobytes = (bytes: number) => (bytes / 1024).toFixed(2);
 
-  function bytesToKilobytes(bytes: any) {
-    const kilobytes = bytes / 1024;
-    return kilobytes;
-  }
-
-  const truncateFileName = (name, maxLength) => {
-    if (name.length > maxLength) {
-      return name.substring(0, maxLength) + "...";
-    }
-    return name;
+  const truncateFileName = (name: string, maxLength: number) => {
+    return name.length > maxLength
+      ? `${name.substring(0, maxLength)}...`
+      : name;
   };
 
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
+        "@media (max-width: 576px)": {
+          padding: "0",
+          gap: "0px",
+        },
+      }}
+    >
+      <h1>Document Upload</h1>
+
       <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
+          gap: "20px",
+          width: "100%",
           justifyContent: "center",
           alignContent: "center",
-          alignItems: "center",
           "@media (max-width: 576px)": {
-            padding: "0",
-            gap: "0px",
+            flexDirection: "column",
+            margin: "8px 0",
           },
         }}
       >
-        <h1>Document Upload</h1>
-
-        <Box
+        <Paragraph
           sx={{
-            display: "flex",
-            gap: "20px",
-            width: "100%",
-            justifyContent: "center",
-            alignContent: "center",
+            fontSize: "20px",
+            fontWeight: 700,
+            color: "#414042",
+            margin: "20px 0",
             "@media (max-width: 576px)": {
-              flexDirection: "column",
-              margin: "8px 0",
+              fontSize: "18px",
+              marginLeft: "0",
             },
           }}
         >
-          <Paragraph
-            sx={{
-              fontSize: "20px",
-              fontWeight: 700,
-              color: "#414042",
-              margin: "20px 0",
-              "@media (max-width: 576px)": {
-                fontSize: "18px",
-                marginLeft: "0",
-              },
-            }}
-          >
-            Please upload any necessary documentation that you would like to
-            accompany the enrollment record
-          </Paragraph>
-        </Box>
+          Please upload any necessary documentation that you would like to
+          accompany the enrollment record
+        </Paragraph>
+      </Box>
 
-        <div
-          onDrop={handleDrop}
-          onDragOver={handleDragOver}
+      <div
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        style={{
+          textAlign: "center",
+          borderRadius: "20px",
+          padding: "20px",
+        }}
+      >
+        <input
+          type="file"
+          name="documents"
+          multiple
+          onChange={handleImageChange}
+          style={{ display: "none" }}
+          id="file-input"
+        />
+        <label
+          htmlFor="file-input"
           style={{
-            textAlign: "center",
-            // border: "2px dashed #ccc",
-            borderRadius: "20px",
-            padding: "20px",
+            cursor: "pointer",
+            boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
           }}
         >
-          <input
-            type="file"
-            name="documents"
-            multiple
-            onChange={handleImageChange}
-            style={{ display: "none" }}
-            id="file-input"
+          <img
+            src={values?.documents?.length > 0 ? UploadImageTwo : UploadImage}
+            alt="upload"
           />
-          <label
-            htmlFor="file-input"
-            style={{
-              cursor: "pointer",
-              boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+        </label>
+      </div>
+      <div>
+        {values?.documents?.map((image: any, index: any) => (
+          <Box
+            key={index}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "10px",
+              margin: "5px 0",
+              padding: "5px",
+              "&:hover": {
+                backgroundColor: "#EEEDED",
+                cursor: "pointer",
+              },
             }}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
           >
-            <img
-              src={values?.documents?.length > 0 ? UploadImageTwo : UploadImage}
-              alt="upload"
-            />
-          </label>
-        </div>
-        <div>
-          {values?.documents?.map((image: any, index: any) => (
             <Box
-              key={index}
+              sx={{
+                color: "#1ECC69",
+                fontSize: "20px",
+                width: "20px",
+                height: "20px",
+              }}
+            >
+              <GoDotFill />
+            </Box>
+            <Box
               sx={{
                 display: "flex",
                 alignItems: "center",
-                gap: "10px",
-                margin: "5px 0",
-                padding: "5px",
-                "&:hover": {
-                  backgroundColor: "#EEEDED",
-                  cursor: "pointer",
-                },
               }}
             >
-              <Box
-                sx={{
-                  color: "#1ECC69",
-                  fontSize: "20px",
-                  width: "20px",
-                  height: "20px",
-                }}
-              >
-                <GoDotFill />
-              </Box>
               <Box
                 sx={{
                   width: "200px",
@@ -158,15 +161,27 @@ const DocumentUpload = ({ formik }: DocumentUploadProps) => {
               >
                 {truncateFileName(image?.name, 30)}
               </Box>
-              <Box>{bytesToKilobytes(image?.size).toFixed(2)} KB</Box>
-              {/* <button type="button" onClick={() => handleRemoveImage(index)}>
-                Remove
-              </button> */}
+              {hoveredIndex === index ? (
+                <Box
+                  onClick={() => handleRemoveImage(index)}
+                  sx={{
+                    color: "#FF1919",
+                    fontSize: "20px",
+                    width: "20px",
+                    height: "20px",
+                    marginLeft: "20px",
+                  }}
+                >
+                  <MdOutlineDeleteOutline />
+                </Box>
+              ) : (
+                <Box>{bytesToKilobytes(image?.size)} KB</Box>
+              )}
             </Box>
-          ))}
-        </div>
-      </Box>
-    </>
+          </Box>
+        ))}
+      </div>
+    </Box>
   );
 };
 
