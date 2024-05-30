2aa9437465c08d32de8febedbc5922ceef20519b
Moazam05 <salmanmoazam08@gmail.com>
2024-05-31 00:00:44 +0500

    constant file add

---
+++ src/constants/enrollmentDataTypes.ts
@@ -0,0 +1,392 @@
+export const genderTypes = [
+  {
+    label: "Male",
+    value: "M",
+  }, 
+  {
+    label: "Female",
+    value: "F",
+  },
+  {
+    label: "Unspecified",
+    value: "U",
+  },
+];
+
+export const phoneNumberTypes = [
+  {
+    label: "Unknown",
+    value: "0",
+  }, 
+  {
+    label: "Home",
+    value: "1",
+  },
+  {
+    label: "Work",
+    value: "2",
+  },
+  {
+    label: "Mobile",
+    value: "3",
+  },  
+];
+
+export const preferredContactTypes = [
+  {
+    label: "Phone",
+    value: "1",
+  },
+  {
+    label: "SMS",
+    value: "2",
+  },
+  {
+    label: "Email",
+    value: "3",
+  },
+  {
+    label: "Mail",
+    value: "4",
+  },  
+];
+
+export const serviceTypes = [
+  {
+    label: "PA Support",
+    value: "1",
+  },
+  {
+    label: "PAP",
+    value: "5",
+  },  
+  {
+    label: "Copay",
+    value: "6",
+  },
+  {
+    label: "Benefits Verification",
+    value: "7",
+  },
+  {
+    label: "Claim Support",
+    value: "11",    
+  },  
+];
+
+export const practiceTypes = [
+  {
+    label: "Freestanding Center/Clinic",
+    value: "2",
+  },
+  {
+    label: "MD Office",
+    value: "4",
+  },  
+  {
+    label: "Other",
+    value: "5",
+  },
+  {
+    label: "Outpatient",
+    value: "7",
+  }, 
+];
+
+export const consentStatusTypes = [
+  {
+    label: "Consented",
+    value: "1",
+  },
+  {
+    label: "Declined",
+    value: "2",
+  },  
+];
+
+export const consentTypes = [
+  {
+    label: "Financial Assistance",
+    value: "1",
+  },
+  {
+    label: "HIPAA",
+    value: "2",
+  },  
+  {
+    label: "HUB Services",
+    value: "3",
+  },  
+  {
+    label: "Manufacturer",
+    value: "4",
+  },  
+  {
+    label: "Marketing",
+    value: "5",
+  },        
+];
+
+export const consentMethodTypes = [
+  {
+    label: "Electronic",
+    value: "6",
+  },
+];
+
+export const consentProvidedByTypes = [
+  {
+    label: "Patient",
+    value: "1",
+  },
+  {
+    label: "Legal Authority",
+    value: "2",
+  },  
+  {
+    label: "HCP",
+    value: "3",
+  },       
+];
+
+export const insurancePlanTypes = [
+  {
+    label: "Commercial",
+    value: "2",
+  },
+  {
+    label: "Federal Employee Program",
+    value: "3",
+  },  
+  {
+    label: "Medicaid",
+    value: "4",
+  },      
+  {
+    label: "Medicare",
+    value: "5",
+  },  
+  {
+    label: "Medi-Gap",
+    value: "6",
+  },  
+  {
+    label: "Other",
+    value: "7",
+  },  
+  {
+    label: "VA/DoD/TriCare",
+    value: "8",
+  },  
+  {
+    label: "Workers Comp",
+    value: "9",
+  },  
+  {
+    label: "Government",
+    value: "10",
+  },
+  {
+    label: "Medicare Advantage",
+    value: "11",
+  },  
+  {
+    label: "Medicare B",
+    value: "12",
+  },  
+  {
+    label: "Medicare D",
+    value: "13",
+  },
+  {
+    label: "Exchange",
+    value: "16",
+  },          
+  {
+    label: "Other Govt",
+    value: "17",
+  },                                     
+];
+
+export const patientsRelationshiptoInsuranceSubscriberTypes = [
+  {
+    label: "Unknown",
+    value: "0",
+  },
+  {
+    label: "Spouse",
+    value: "1",
+  },  
+  {
+    label: "Other",
+    value: "5",
+  },   
+  {
+    label: "Self",
+    value: "8",
+  },  
+  {
+    label: "Dependent",
+    value: "9",
+  },           
+];
+
+export const productCodeTypes = [
+  {
+    label: "NDC",
+    value: "1",
+  },
+  {
+    label: "UPC",
+    value: "2",
+  },  
+  {
+    label: "J Code",
+    value: "3",
+  },   
+  {
+    label: "CPT Code",
+    value: "4",
+  },  
+  {
+    label: "C Code",
+    value: "5",
+  },      
+  {
+    label: "Q Code",
+    value: "6",
+  },        
+];
+
+export const caregiverRelationshiptoPatientTypes = [
+  {
+    label: "Caregiver",
+    value: "1",
+  },
+  {
+    label: "Spouse",
+    value: "2",
+  },  
+  {
+    label: "Family Member",
+    value: "3",
+  },   
+  {
+    label: "Legal Guardian",
+    value: "4",
+  },  
+  {
+    label: "Power of Attorney",
+    value: "5",
+  },            
+];
+
+
+export const attachmentTypes = [
+  {
+    label: "Appeal",
+    value: "1",
+  },
+  {
+    label: "Clinical",
+    value: "2",
+  },  
+  {
+    label: "Enrollment Form",
+    value: "3",
+  },   
+  {
+    label: "EOB",
+    value: "4",
+  },  
+  {
+    label: "Financial",
+    value: "5",
+  },        
+  {
+    label: "Consent",
+    value: "6",
+  },
+  {
+    label: "Insurance",
+    value: "7",
+  },  
+  {
+    label: "PAP",
+    value: "8",
+  },   
+  {
+    label: "Prescription",
+    value: "9",
+  },  
+  {
+    label: "Prior Authorization",
+    value: "10",
+  },            
+  {
+    label: "Uploaded File",
+    value: "12",
+  },        
+  {
+    label: "Medical Necessity",
+    value: "13",
+  },
+  {
+    label: "Coversheet",
+    value: "14",
+  },  
+  {
+    label: "Patient Authorization",
+    value: "15",
+  },   
+  {
+    label: "Copay",
+    value: "16",
+  },  
+  {
+    label: "Start Form",
+    value: "17",
+  },     
+];
+
+export const priorAuthorizationStatusTypes = [
+  {
+    label: "Approved",
+    value: "1",
+  },
+  {
+    label: "Cancelled",
+    value: "2",
+  },  
+  {
+    label: "Complete",
+    value: "3",
+  },   
+  {
+    label: "Denied",
+    value: "4",
+  },  
+  {
+    label: "Initiated",
+    value: "5",
+  },        
+  {
+    label: "Missing Information",
+    value: "6",
+  },  
+  {
+    label: "Pending",
+    value: "7",
+  },         
+];
+
+export const dispenseTypes = [
+  {
+    label: "IOD (In Office Dispense) / Buy and Bill",
+    value: "1",
+  },
+  {
+    label: "Specialty Pharmacy",
+    value: "2",
+  },      
+];
