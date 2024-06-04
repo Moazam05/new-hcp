3a3e0d8c513e61d5fb746c22900c81d430e1e63b
Moazam05 <salmanmoazam08@gmail.com>
2024-06-05 01:42:41 +0500

    submit form

--- src/views/PatientManagement/PatientTypes/CaseOne/index.tsx
+++ src/views/PatientManagement/PatientTypes/CaseOne/index.tsx
@@ -15,6 +15,7 @@ import DocumentUpload from "../../components/steps/DocumentUpload";
 import FinancialAssistant from "../../components/steps/FinancialAssistant";
 import AssistanceAttestation from "../../components/steps/AssistanceAttestation";
 import Attestation from "../../components/steps/Attestation";
+import Submit from "./components/Submit";
 
 const newSteps = [
   MedicalInsurance,
@@ -29,14 +30,15 @@ const CaseOne = () => {
   const therapyTypes = localStorage.getItem("therapy");
   // states
   const [activeStep, setActiveStep] = useState<any>(0);
+  const [showSubmitForm, setShowSubmitForm] = useState(false);
 
-  const isLastStep = () => {
-    return activeStep === newSteps.length - 1;
-  };
+  // const isLastStep = () => {
+  //   return activeStep === newSteps.length - 1;
+  // };
 
-  const handlePrev = () => {
-    setActiveStep(Math.max(activeStep - 1, 0));
-  };
+  // const handlePrev = () => {
+  //   setActiveStep(Math.max(activeStep - 1, 0));
+  // };
 
   const handleNext = () => [
     setActiveStep(Math.min(activeStep + 1, newSteps.length - 1)),
@@ -56,14 +58,18 @@ const CaseOne = () => {
   const onSubmit = async (values: any, formikBag: any) => {
     const { setSubmitting, setTouched } = formikBag;
 
-    if (activeStep >= 0 && activeStep <= 6) {
+    if (activeStep >= 0 && activeStep <= 4) {
       handleNext();
       setTouched(false);
     }
 
-    if (isLastStep()) {
-      alert("Form submitted successfully");
+    if (activeStep === 5) {
+      setShowSubmitForm(true);
     }
+
+    // if (isLastStep()) {
+    //   alert("Form submitted successfully");
+    // }
 
     setSubmitting(false);
   };
@@ -91,62 +97,64 @@ const CaseOne = () => {
         />
         <img src={PrescriberAttestation} alt="step" />
       </Box>
-
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignContent: "center",
-          alignItems: "center",
-          margin: "50px 100px",
-          "@media (max-width: 992px)": {
-            margin: "20px",
-          },
-        }}
-      >
+      {showSubmitForm ? (
+        <Submit />
+      ) : (
         <Box
           sx={{
-            width: fullWidthSteps.includes(activeStep) ? "100%" : "800px",
-            "@media (max-width: 576px)": {
-              width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center",
+            margin: "50px 100px",
+            "@media (max-width: 992px)": {
+              margin: "20px",
             },
           }}
         >
-          <Formik
-            initialValues={initialValues}
-            onSubmit={onSubmit}
-            validationSchema={validationSchema}
-            // validateOnBlur={false}
-            // validateOnChange={false}
+          <Box
+            sx={{
+              width: fullWidthSteps.includes(activeStep) ? "100%" : "800px",
+              "@media (max-width: 576px)": {
+                width: "100%",
+              },
+            }}
           >
-            {(props: any) => (
-              <Form>
-                {React.createElement(newSteps[activeStep], {
-                  formik: props,
-                })}
+            <Formik
+              initialValues={initialValues}
+              onSubmit={onSubmit}
+              validationSchema={validationSchema}
+              // validateOnBlur={false}
+              // validateOnChange={false}
+            >
+              {(props: any) => (
+                <Form>
+                  {React.createElement(newSteps[activeStep], {
+                    formik: props,
+                  })}
 
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    marginTop: "80px",
-                    "@media (max-width: 576px)": {
-                      marginTop: "40px",
-                    },
-                  }}
-                >
-                  {activeStep !== 0 && (
-                    <Button
-                      variant="contained"
-                      color="error"
-                      onClick={handlePrev}
-                    >
-                      BACK
-                    </Button>
-                  )}
-                  <PrimaryButton type="submit">
-                    {/* {isLoading ? (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      marginTop: "80px",
+                      "@media (max-width: 576px)": {
+                        marginTop: "40px",
+                      },
+                    }}
+                  >
+                    {/* {activeStep !== 0 && (
+                      <Button
+                        variant="contained"
+                        color="error"
+                        onClick={handlePrev}
+                      >
+                        BACK
+                      </Button>
+                    )} */}
+                    <PrimaryButton type="submit">
+                      {/* {isLoading ? (
                       <Box
                         sx={{
                           padding: "4px 20px",
@@ -157,24 +165,26 @@ const CaseOne = () => {
                     ) : (
                       "CONTINUE"
                     )} */}
-                    CONTINUE
-                  </PrimaryButton>
-                </Box>
-              </Form>
-            )}
-          </Formik>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              margin: "15px 0 0",
-            }}
-            // onClick={() => setModalOpen(true)}
-          >
-            <SecondaryButton />
+                      CONTINUE
+                    </PrimaryButton>
+                  </Box>
+                </Form>
+              )}
+            </Formik>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                margin: "15px 0 0",
+              }}
+              // onClick={() => setModalOpen(true)}
+            >
+              <SecondaryButton />
+            </Box>
           </Box>
         </Box>
-      </Box>
+      )}
+
       <Footer />
     </>
   );
